c2f9b9ee30c2cebef184f32ad20fbe3535da0944
Omid SoheilNia <soheilniao@gmail.com>
2022-12-12 15:45:46 +0330

    nodejs finished

--- BicycleShop/index.js
+++ BicycleShop/index.js
@@ -8,22 +8,41 @@ const server = http.createServer(async (req, res) => {
   const myUrl = new URL(req.url, `http://${req.headers.host}/`);
   const patchname = myUrl.pathname;
   const id = myUrl.searchParams.get("id");
-  //   console.log(patchname, id, user);
+  const bicycles = require("./data.json");
+
   if (patchname === "/") {
-    const html = await fs.readFile("./view/bicycles.html", "utf-8");
+    let html = await fs.readFile("./view/bicycles.html", "utf-8");
+    let AllMainBicycles = await fs.readFile("./view/main/bmain.html", "utf-8");
+
+    let allTheBicycles = "";
+
+    for (let index = 0; index < 6; index++) {
+      allTheBicycles += reMap(AllMainBicycles, bicycles[index]);
+    }
+
+    html = html.replace(/<%AllMainBicycles%>/g, allTheBicycles);
+
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
   } else if (patchname === "/bicycle" && id >= 0 && id <= 5) {
-    const html = await fs.readFile("./view/overview.html", "utf-8");
+    let html = await fs.readFile("./view/overview.html", "utf-8");
+
+    const bicycle = bicycles.find((b) => b.id === id);
+
+    html = html.replace(/<%NAME%>/g, bicycle.name);
+
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<div><h1> File Not Found.</h1></div>");
   }
-
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.end("<h1>Server Is Up..</h1>");
 });
 
 server.listen(4000);
+
+const reMap = (html, bicycle) => {
+  html = html.replace(/<%ID%>/g, bicycle.id);
+  html = html.replace(/<%NAME%>/g, bicycle.name);
+  return html;
+};
