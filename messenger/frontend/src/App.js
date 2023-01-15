bd6bba133eb513598ef472880e7f3cb1b0007cbc
Omid SoheilNia <soheilniao@gmail.com>
2023-01-15 17:03:25 +0330

    folder 39 - messenger page added to root

--- messenger/frontend/src/App.js
+++ messenger/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LogIn from "./components/LogIn";
+import Messenger from "./components/Messeger";
 import Register from "./components/Register";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/messenger/login" element={<LogIn />} />
           <Route path="/messenger/register" element={<Register />} />
+          <Route path="/" element={<Messenger />} />
         </Routes>
       </BrowserRouter>
     </div>
